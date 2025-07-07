33320e3763457491513785a4b39da5c7344a6e78
Martin-Rojas <rojas.martin25@yahoo.com>
2025-07-06 17:53:42 -0700

    code updated to add a project and connect the list todos.

--- src/projectManager.js
+++ src/projectManager.js
@@ -1,11 +1,45 @@
 import addProjectTitle from "./projectTitlesUI";
 
+function idGenerator() {
+  return Math.random().toString(36).substring(2, 10);
+}
 const projectList = document.getElementById(`project-list`);
-const projects = [`Project 01`, `Project02`, `Project03`];
+const projects = [
+  {
+    projectID: idGenerator(),
+    projectName: `Web-App`,
+    todos: [
+      {
+        id: idGenerator(),
+        title: `Design UI`,
+        dueDate: `2025-06-20`,
+        priority: `high`,
+      },
+    ],
+  },
+  {
+    projectID: idGenerator(),
+    projectName: `Portafolio-App`,
+    todos: [
+      {
+        id: idGenerator(),
+        title: `Design UI`,
+        dueDate: `2025-06-18`,
+        priority: `medium`,
+      },
+      {
+        id: idGenerator(),
+        title: `plan projects`,
+        dueDate: `2025-06-18`,
+        priority: `high`,
+      },
+    ],
+  }
+];
 
 function displayProjectNames() {
   projects.forEach((project) => {
-    addProjectTitle(project);
+    addProjectTitle(project.projectName);
   });
 }
 
@@ -14,5 +48,9 @@ function addProject(projectTitle) {
   projects.push(projectTitle);
   displayProjectNames();
 }
+
+/* When the last item in the todo list is delete 
+   the project should be delete it*/
+function deleteProject() {}
 
 export { addProject, displayProjectNames };
