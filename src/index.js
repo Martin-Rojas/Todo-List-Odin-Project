0aeb6662c3acf81c5f596a5e860e9aef24349be7
Martin-Rojas <rojas.martin25@yahoo.com>
2025-07-15 17:28:40 -0700

    Clean code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { addProject, displayProjectNames, readProject } from "./projectManager";
 import createTodoUI from "./todoUI";
 
 const btnAddProject = document.getElementById(`add-project`);
-let activeProjectID = `ax4wrtma`;
 
 // Handle the click of the project titles
 const projectList = document.getElementById(`project-list`);
@@ -38,21 +37,16 @@ btnAddProject.addEventListener("click", () => {
 
 projectList.addEventListener("click", (e) => {
   if (e.target.classList.contains("project-title")) {
+    // set activeProjectID
     const projectID = e.target.dataset.projectId;
     console.log("Clicked project ID:", projectID);
 
-    console.log(readProject(projectID).todos);
-
-    // Now you can use this ID to:
-    // - set activeProjectID
-    // - show todos for that project
     // display todos
     const todos = readProject(projectID).todos;
     const todosItems = document.getElementById(`todos-itmes`);
     todosItems.innerHTML = ``;
 
     todos.forEach((todo) => {
-      console.log(todo);
       createTodoUI(todo);
     });
     // - add new todos to the right list
